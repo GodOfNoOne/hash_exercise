07d114e628fd7cad4638e901e9651a4977eebe02
GodOfNoOne <115725718+GodOfNoOne@users.noreply.github.com>
2023-11-07 14:27:40 +0200

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,16 +1,30 @@
 import hashlib
 import csv
-def fill_table(start='aaaaaa',stop='zzzzzz'):
-    if ord(start[-1])+1<= ord('z'):
-        start_list=list(start)
-        start_list[-1]=chr(ord(start_list[-1]) + 1)
-        start=''.join(start_list)
-        print(start)
-    
+import itertools
+def fill_table2(num_of_letters):
+    if num_of_letters < 2 or num_of_letters > 6:
+        raise ValueError("Number of letters should be between 2 and 6")
+
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    combinations = []
+
+    for combination in itertools.product(alphabet, repeat=num_of_letters):
+        combinations.append(''.join(combination))
+
+    return combinations
+
+def fill_table(start, stop):
+    combinations=[]
+    for num_of_letters in range(start, stop):
+        temp_combinations = fill_table2(num_of_letters)
+        combinations+=temp_combinations
+    print('YEAHHHHHH BABYYYYYYY')
+    return combinations
+
+
+            
+
 with open ('hash_table.csv','w',newline="") as file:
     csvwriter = csv.writer(file)
     csvwriter.writerow(['password','md5'])
-    fill_table()
-    
-
-
+    csvwriter.writerows(fill_table(6,7))
