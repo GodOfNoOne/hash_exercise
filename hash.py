c837a71d11b09a37bdccda3ce4e7babed309f1b9
GodOfNoOne <115725718+GodOfNoOne@users.noreply.github.com>
2023-11-07 15:55:17 +0200

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,30 +1,28 @@
 import hashlib
 import csv
-import itertools
-def fill_table2(num_of_letters):
-    if num_of_letters < 2 or num_of_letters > 6:
-        raise ValueError("Number of letters should be between 2 and 6")
-
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    combinations = []
-
-    for combination in itertools.product(alphabet, repeat=num_of_letters):
-        combinations.append(''.join(combination))
-
-    return combinations
-
 def fill_table(start, stop):
-    combinations=[]
-    for num_of_letters in range(start, stop):
-        temp_combinations = fill_table2(num_of_letters)
-        combinations+=temp_combinations
-    print('YEAHHHHHH BABYYYYYYY')
+    if len(start) != len(stop):
+        raise ValueError("Start and stop strings must have the same length")
+
+    def generate_combinations(current, index):
+        if index == len(current):
+            combinations.append(''.join(current))
+            return
+
+        for char in range(ord(start[index]), ord(stop[index]) + 1):
+            current[index] = chr(char)
+            generate_combinations(current, index + 1)
+
+    combinations = []
+    generate_combinations(list(start), 0)
+
     return combinations
-
-
-            
 
 with open ('hash_table.csv','w',newline="") as file:
     csvwriter = csv.writer(file)
     csvwriter.writerow(['password','md5'])
-    csvwriter.writerows(fill_table(6,7))
+    comb_list=fill_table("aaaaaa","zzzzzz")
+    insert=[]
+    for c in comb_list:
+        insert.append([c,hashlib.md5(c.encode()).hexdigest()])
+    csvwriter.writerows(insert)
