b2991c16278faf924d4cd5cbe25d4c6668c2452d
GodOfNoOne <115725718+GodOfNoOne@users.noreply.github.com>
2023-11-09 18:20:06 +0200

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,28 +1,40 @@
 import hashlib
-import csv
-def fill_table(start, stop):
-    if len(start) != len(stop):
-        raise ValueError("Start and stop strings must have the same length")
 
-    def generate_combinations(current, index):
-        if index == len(current):
-            combinations.append(''.join(current))
-            return
+def find_password(start, stop, num_of_letters, md5_password):
+    all_characters = [chr(i) for i in range(ord('a'), ord('z')+1)]
+#[chr(i) for i in range(ord('A'), ord('Z')+1)]
+#[chr(i) for i in range(ord('0'), ord('9')+1)]
 
-        for char in range(ord(start[index]), ord(stop[index]) + 1):
-            current[index] = chr(char)
-            generate_combinations(current, index + 1)
 
-    combinations = []
-    generate_combinations(list(start), 0)
 
-    return combinations
+# maybe make num of letters a tupple for range
 
-with open ('hash_table.csv','w',newline="") as file:
-    csvwriter = csv.writer(file)
-    csvwriter.writerow(['password','md5'])
-    comb_list=fill_table("aaaaaa","zzzzzz")
-    insert=[]
-    for c in comb_list:
-        insert.append([c,hashlib.md5(c.encode()).hexdigest()])
-    csvwriter.writerows(insert)
+
+    def generate_combinations(code, num_of_letters):
+        if len(code) == num_of_letters:
+            md5_hash = hashlib.md5(code.encode()).hexdigest()
+            if md5_hash == md5_password:
+                return code
+        elif len(code) < num_of_letters:
+            for char in all_characters:
+                new_code = code + char
+                result = generate_combinations(new_code, num_of_letters)
+                if result:
+                    return result
+
+    for char in all_characters[all_characters.index(start):all_characters.index(stop)+1]:
+        result = generate_combinations(char, num_of_letters)
+        if result:
+            return result
+
+    return None
+
+start = 'a'
+stop = 'b'
+num_of_letters = 3
+md5_password = '45a944c4b06807c66113939489658873'
+result = find_password(start, stop, num_of_letters, md5_password)
+if result:
+    print(result)
+else:
+    print("Password not found.")
